da7131c3f618191162dab74478ec328c32f62f4b
Joseph Bauer <211513@corpaa.aa.com>
2021-10-07 16:34:15 -0400

    Initial commit.  Just fucking around.Add project files.

---
+++ PubtracSSPoc/Data/Copyholder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PubtracSSPoc.Data
+{
+    public class Copyholder
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Station { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Company { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Remarks { get; set; }
+
+    }
+}
---
+++ PubtracSSPoc/Data/CopyholderService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PubtracSSPoc.Data
+{
+    public class CopyholderService
+    {
+        #region Property
+        private readonly ApplicationDbContext _appDBContext;
+        #endregion
+
+        #region Constructor
+        public CopyholderService(ApplicationDbContext appDBContext)
+        {
+            _appDBContext = appDBContext;
+        }
+        #endregion
+
+        #region Get List of Copyholders
+        public async Task<List<Copyholder>> GetAllCopyholdersAsync()
+        {
+            return await _appDBContext.Copyholders.ToListAsync();
+        }
+        #endregion
+
+        #region Insert Copyholder
+        public async Task<bool> InsertCopyholderAsync(Copyholder copyholder)
+        {
+            await _appDBContext.Copyholders.AddAsync(copyholder);
+            await _appDBContext.SaveChangesAsync();
+            return true;
+        }
+        #endregion
+
+        #region Get Copyholder by Id
+        public async Task<Copyholder> GetCopyholderAsync(int Id)
+        {
+            Copyholder copyholder = await _appDBContext.Copyholders.FirstOrDefaultAsync(c => c.Id.Equals(Id));
+            return copyholder;
+        }
+        #endregion
+
+        #region Update Copyholder
+        public async Task<bool> UpdateCopyholderAsync(Copyholder copyholder)
+        {
+            _appDBContext.Copyholders.Update(copyholder);
+            await _appDBContext.SaveChangesAsync();
+            return true;
+        }
+        #endregion
+
+        #region DeleteCopyholder
+        public async Task<bool> DeleteCopyholderAsync(Copyholder copyholder)
+        {
+            _appDBContext.Remove(copyholder);
+            await _appDBContext.SaveChangesAsync();
+            return true;
+        }
+        #endregion
+    }
+}
